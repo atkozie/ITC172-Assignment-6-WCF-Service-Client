5c3909b57f1917a36d866d3640c5e9ac718a4121
atkozie <atkozie@gmail.com>
2017-03-13 11:12:39 -0700

    Initial commit
    some code is commented out as per our conversation, it was returning a mysterious error on line 38.

---
+++ WebSite1/GrantRequest.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GrantRequest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+        
+    {
+        if (Session["userkey"] == null)
+        {
+            Response.Redirect("Default.aspx");
+        }
+   
+
+    GetGrants();
+    }
+    protected void GetGrants()
+    {
+        CAServiceReference.CAServiceClient cas = new CAServiceReference.CAServiceClient();
+        int key = (int)Session["userkey"];
+        CAServiceReference.GrantInfo[] grants = cas.GetGrantsByPerson(key);
+        GridView.DataSource = grants;
+        GridView.DataBind();
+    }
+}
+
